8e2e842ff26e3bdacdf8009e8478017d46bb7204
ProwlingLynx <tamezjavier@gmail.com>
2022-11-22 23:36:16 -0600

    implementing filter for namesList.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static int[] filterIntsGreaterThanTen (int[] nums) {
@@ -17,9 +19,55 @@ public class Main {
             System.out.println(val);
         }
     }
+    public static void printAllElements(ArrayList<String> list) {
+        for (String val: list) {
+            System.out.println(val);
+        }
+    }
+
+    public static void shuffleList(ArrayList<String> list) {
+        Collections.shuffle(list);
+    }
+    public static void createLineBreak() {
+        System.out.println("\n<<<<<------------------------------->>>>>\n");
+    }
+
+    public static void filterLessThanFiveChars(ArrayList<String> names) {
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            if (name.length() < 5) {
+                names.remove(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int[] testCaseOne = {3,5,1,2,7,9,8,13,25,32};
         printAllElements(filterIntsGreaterThanTen(testCaseOne));
+
+
+        createLineBreak();
+        ArrayList<String> namesList = new ArrayList<>();
+        namesList.add("Nancy");
+        namesList.add("Jinichi");
+        namesList.add("Fujibayashi");
+        namesList.add("Momochi");
+        namesList.add("Ishikawa");
+        System.out.println("Before Shuffling the arrayList:\n");
+
+        printAllElements(namesList);
+
+        shuffleList(namesList);
+        System.out.println("And now after shuffling!\n");
+        printAllElements(namesList);
+        createLineBreak();
+
+        System.out.println("Filtering out all names less than 5 chars in length\n");
+        filterLessThanFiveChars(namesList);
+        printAllElements(namesList);
+        createLineBreak();
+
+
     }
 }
